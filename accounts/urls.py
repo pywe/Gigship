27dd51fd07ce189279197323ea0228edc139d327
teddy <teddahissiho@gmail.com>
2020-01-14 21:07:26 +0000

    Index Page done for the Gigship Project

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index,name="home"),
+    url(r'^how-it-works/$', views.how_it_works,name="how-it-works"),
+    url(r'^registration/$', views.registration,name="registration"),
+    url(r'^login/$', views.login,name="login"),
+    url(r'^forgot-password/$', views.forgot,name="forgot-password"),
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 # index page, showing homepage to users
 def index(request):
-    template_name = "accounts/intro-page.html"
+    template_name = "accounts/index.html"
+    args = {}
+    return render(request,template_name,args)
+
+
+# how it works page, showing how it works page to users
+def how_it_works(request):
+    template_name = "accounts/how-it-works.html"
+    args = {}
+    return render(request,template_name,args) 
+
+
+# registration page, showing registration page to users
+def registration(request):
+    template_name = "accounts/registration.html"
+    args = {}
+    return render(request,template_name,args)
+
+# login page, showing login page to users
+def login(request):
+    template_name = "accounts/login.html"
+    args = {}
+    return render(request,template_name,args)
+
+# forgot password page, showing forgot password page to users
+def forgot(request):
+    template_name = "accounts/forgot.html"
     args = {}
     return render(request,template_name,args)
