7d4564d6dd9dc4ab4cb5e4fbbff7967fb1e7d5d2
TheoElia <theophylusnhutiphapha@gmail.com>
2020-01-19 13:29:30 +0000

    ...

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,12 +10,7 @@ urlpatterns = [
     url(r'^faq/$', views.faq,name="faq"),
     url(r'^create-resume/$', views.create_resume,name="create-resume"),
     url(r'^support/$', views.support,name="support"), 
-<<<<<<< HEAD
     url(r'^post-job/$', views.post_job,name="post-job"),
     url(r'^accounts/create-freelancer/$', views.create_freelancer,name="create-freelancer"),
     url(r'^accounts/create-employer/$', views.create_employer,name="create-employer"),  
-=======
-    url(r'^post-job/$', views.post_job,name="post-job"),  
-    url(r'^dashboard/$', views.dashboard,name="dashboard"),  
->>>>>>> 7a6047e951fd1907ae6d09017ed2877e555897e8
     ]
