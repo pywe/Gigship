7b8da151a003d799195274116c3ab972ee9a2836
TheoElia <theophylusnhutiphapha@gmail.com>
2020-03-26 11:17:49 +0000

    files display added

--- static/js/search.js
+++ static/js/search.js
@@ -5,6 +5,31 @@ function exists(ele) {
         return false
     }
 }
+function Image(path){
+    // var fileInput = document.getElementById(id+"file");
+    // var filePath = fileInput.value;
+    var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif|\.svg)$/i;
+    if(allowedExtensions.exec(path)){
+        // alert('This is an image');
+        // fileInput.value = '';
+        return true;
+    }else{
+        return false
+    }
+}
+
+function Video(path){
+    // var fileInput = document.getElementById(id+"file");
+    // var filePath = fileInput.value;
+    var allowedExtensions = /(\.mp4|\.mpeg|\.webm)$/i;
+    if(allowedExtensions.exec(path)){
+        // alert('This is a video');
+        // fileInput.value = '';
+        return true;
+    }else{
+        return false
+    }
+}
 
 function search(data) {
     var url = window.location.origin + "/gigs/search-gigs/";
@@ -25,9 +50,13 @@ function search(data) {
             // look for id of element that will hold the results and append to it
             // objects received from calling api
             $("#found-gigs").text("")
-            for (var i = 0; i < json_resp.objects.length; i++) {
+            if((json_resp.objects).length>0){
+            for (var i = 0; i < (json_resp.objects).length; i++) {
                 var object = json_resp.objects[i]
                 var card = constructCard(object);
+                $("#found-gigs").append(card)
+            }}else{
+                var card = `<p>Sorry, No Gigs Found matching your query. You could try using categories</p>`
                 $("#found-gigs").append(card)
             }
             $('.scrolling-wrapper').slick({
@@ -35,30 +64,58 @@ function search(data) {
                 slidesToScroll: 1,
                 autoplay: true,
                 autoplaySpeed: 3000,
+                adaptiveHeight: true
             });
+            // hide loader
+            $("#global-loader").removeClass('show');
+            $("#global-loader").addClass('hidden');
 
         }
         console.log(json_resp)
     }
 }
 
+// TODO: remember to remove trailing slash in production
+function contructFiles(files){
+    var div = ``
+    for (var i=0;i<files.length;i++){
+        if(Image(files[i])){
+            div += `<div class="img">
+            <img src="${files[i]}" alt="img">
+        </div>`
+        }else{
+            div += `<div class="img">
+            <video alt="video" controls>
+            <source src="${files[i]}">
+            </video>
+        </div>`
+        }
+    }
+    return div
+}
+
 function constructCard(object) {
     var service = object.service
     var start_price = object.start_price
     var comment = object.detail
+    var files = object.files
+    var fileshtml = contructFiles(files);
+    var temp = `<div class="scrolling-wrapper">
+    <div class="img">
+        <img src="/static/services/bnw.png" alt="img">
+    </div>
+    <div class="img">
+        <img src="/static/services/logo_color.png" alt="img">
+    </div>
+    <div class="img">
+        <img src="/static/images/products/products/ed1.jpg" alt="img">
+    </div>
+    </div>`
     var html = `<div class="col-xl-4 col-md-6">
     <div class="card">
-        <div class="scrolling-wrapper">
-            <div class="img">
-                <img src="/static/services/bnw.png" alt="img">
-            </div>
-            <div class="img">
-                <img src="/static/services/logo_color.png" alt="img">
-            </div>
-            <div class="img">
-                <img src="/static/images/products/products/ed1.jpg" alt="img">
-            </div>
-        </div>
+    <div class="scrolling-wrapper">
+        ${fileshtml}
+    </div>
         <div class="card-body">
             <div class="item-card7-desc">
                 <div class="item-card7-text">
@@ -89,7 +146,7 @@ function constructCard(object) {
 }
 
 function execute_search() {
-    var search_q = localStorage.getItem('query')
+    var search_q = localStorage.getItem('query');
     if (exists(search_q)) {
         $("#query").val(JSON.parse(search_q).q)
         search(JSON.parse(search_q))
@@ -103,6 +160,8 @@ document.getElementById('query').onkeyup = function (e) {
 }
 // when search button at home page is clicked
 $('#search-btn').on("click", function (event) {
+    // show loader
+    $("#global-loader").addClass('show');
     // we save what the user searched for
     var query = $("#query").val()
     var category = ""
